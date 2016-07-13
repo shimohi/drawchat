25d54c63d948d4438dac1c2a15857aecd60dc356
斎藤 吉和 <sar.jp3@gmail.com>
2016-07-14 07:15:18 +0900

    update dos

--- src/dts/drawchat-updator.d.ts
+++ src/dts/drawchat-updator.d.ts
@@ -2,30 +2,117 @@ declare namespace drawchat.updator {
 
 	interface Updator{
 
+		/**
+		 * レイヤーを追加し、追加されたレイヤーのIDを取得する。
+		 */
+		addLayer():string;
 
+		/**
+		 * 指定されたレイヤーを削除する。
+		 * @param layerId
+		 */
+		removeLayer(layerId:string):void;
 
-		//commit():void;
+		/**
+		 * レイヤーIDのリストを取得する。
+		 */
+		getLayers():string[];
 
-		undo():boolean;
+		/**
+		 * 変形トランザクションを開始する。
+		 */
+		beginTransform():TransformTransaction;
 
-		redo():boolean;
+		/**
+		 * 切り抜きトランザクションを開始する。
+		 * @param layerId
+		 */
+		beginClip(layerId:string):ClipTransaction;
+
+		/**
+		 * 描画トランザクションを開始する。
+		 * @param layerId
+		 */
+		beginPath(layerId:string):PathTransaction;
+
+		/**
+		 * テキストトランザクションを開始する。
+		 * @param layerId
+		 */
+		beginText(layerId:string):TextTransaction;
+
+		/**
+		 * 表示順変更トランザクションを開始する。
+		 * @param layerId
+		 */
+		beginChangeSequence(layerId:string):ChangeSequenceTransaction;
+
+		/**
+		 * Undoが可能かどうかの判定
+		 */
+		canUndo():boolean;
+
+		/**
+		 * Redoが可能かどうかの判定
+		 */
+		canRedo():boolean;
+
+		/**
+		 * 一つ前の状態に戻す。
+		 */
+		undo():void;
+
+		/**
+		 * 戻した変更を進める。
+		 */
+		redo():void;
 	}
 
-	interface ClipSession{
+	/**
+	 * 変形トランザクション
+	 */
+	interface TransformTransaction{
+
+		setMatrix(transform:Transform):void;
+
+		rollback():void;
 
 		commit():void;
 	}
 
-	interface StrokeSession{
-
+	/**
+	 * 切り抜きトランザクション
+	 */
+	interface ClipTransaction{
+		rollback():void;
 		commit():void;
 	}
 
-	interface SequenceSession{
+	/**
+	 * 線描画トランザクション
+	 */
+	interface PathTransaction{
+
+		rollback():void;
 		commit():void;
 	}
 
+	/**
+	 * 全面、背面入れ替えのトランザクション
+	 */
+	interface ChangeSequenceTransaction{
 
 
 
+		rollback():void;
+		commit():void;
+	}
+
+	/**
+	 * テキスト編集トランザクション
+	 */
+	interface TextTransaction{
+		rollback():void;
+		commit():void;
+	}
 }
