275eaf8b0bc9a94eca1b977e84ab13707db70c43
y.saito <yo_saito@kyodoprinting.co.jp>
2016-08-18 19:33:36 +0900

    try react toolbox

--- src/ts/entry.tsx
+++ src/ts/entry.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import FontIcon from 'react-toolbox/lib/font_icon';
+
 // import {Card, CardTitle, CardMedia, CardText, CardActions} from "react-toolbox";
 // import {Button} from "react-toolbox";
 
@@ -13,14 +14,28 @@ export interface IMainState {
 
 export interface IMainProps {}
 class Root extends React.Component<IMainProps, IMainState> {
+
 	constructor(props:IMainProps) {
 		super(props);
 	}
+
 	render() {
+		var divStyle = {
+			'fontSize': '48px'
+		};
 		return(
+
+
 			<span>
-    			<FontIcon value='add' />
-    			<FontIcon value='favorite' />
+    			<FontIcon style={divStyle} value='brush' />
+    			<FontIcon value='brush' />
+    			<FontIcon value='brush' />
+    			<FontIcon value='format_color_fill' />
+    			<FontIcon value='text_fields' />
+    			<FontIcon value='brush' />
+    			<FontIcon value='crop_free' />
+    			<FontIcon value='colorize' />
+    			<FontIcon value='panorama_fish_eye' />
     			<FontIcon>star</FontIcon>
   			</span>
 			// <Card style={{width: '350px'}}>
