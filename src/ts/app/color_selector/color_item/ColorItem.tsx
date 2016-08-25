5c1aa065f5839e41f10b638950a53d3687a920d5
y.saito <yo_saito@kyodoprinting.co.jp>
2016-08-25 13:43:48 +0900

    try react state

--- src/ts/app/color_selector/color_item/ColorItem.tsx
+++ src/ts/app/color_selector/color_item/ColorItem.tsx
@@ -1,25 +1,45 @@
 import * as React from 'react';
 import * as styles from './ColorItemStyle.scss';
+import Color = drawchat.editor.Color;
+import DrawchatEditorProperties = drawchat.editor.DrawchatEditorProperties;
 
-export interface IMainState {
-	newItem?: {
-		description: string;
-	};
-	todoList?: string[];
+export interface ColorItemProps{
+	color:Color;
+	onSelect:SelectColorHandler
+}
+export interface SelectColorHandler{
+	(color:Color):void;
 }
 
-export interface IMainProps {}
-export class ColorItem extends React.Component<IMainProps, IMainState> {
-	constructor(props:IMainProps) {
+export class ColorItem extends React.Component<ColorItemProps, ColorItemProps> {
+
+	constructor(props:ColorItemProps) {
 		super(props);
-		this.state = {};
+		this.state = props;
 	}
+
+	// componentDidMount(){
+	// }
+
+	selectColor(
+		// e:React.SyntheticEvent
+	):void{
+		if(!this.state.onSelect){
+			return;
+		}
+		this.state.onSelect(this.state.color);
+	}
+
 	render(){
+		let color = this.state.color ? this.state.color :{r:0,g:0,b:0};
+		var spanStyle= {
+			color:`rgb(${color.r},${color.g},${color.b})`
+		};
 		return(
 			<div className={styles.item}>
-				<div className={styles.item__circle}>
+				<div  onClick={(event) => this.selectColor()} className={styles.item__circle}>
 					<div className={styles.item__circle_cell}>
-						<span className="material-icons">format_color_fill</span>
+						<span className="material-icons" style={spanStyle}>format_color_fill</span>
 					</div>
 				</div>
 			</div>
